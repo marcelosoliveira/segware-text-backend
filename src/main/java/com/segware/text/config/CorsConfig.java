c6ad62cbe8fda0fa07bcf21567ad7877009a2f3b
marcelosoliveira <msbobsk8@gmail.com>
2021-06-27 18:47:08 -0300

    Configurando o cors da API

--- src/main/java/com/segware/text/config/CorsConfig.java
+++ src/main/java/com/segware/text/config/CorsConfig.java
@@ -2,11 +2,13 @@ package com.segware.text.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 public class CorsConfig {
 
     @Bean
